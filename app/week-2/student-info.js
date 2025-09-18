85438b3325df47c3ca302dbd0bebc3da72d7ab82
Temi Bankole <temibankole5@gmail.com>
2025-09-18 17:16:28 -0600

    fixing link problems

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,18 +1,18 @@
+//week 2 page
 'use client';
 
 import Link from 'next/link';
 
-const StudentInfo = () => {
+export default function StudentInfo() {
   return (
-    <div>
-      <h2>Name: Temi Bankole</h2>
+    <main>
+      <h1>Shopping List</h1>
+      <h1>Name: Temi Bankole</h1>
       <p>
         Github:
-        <Link href="https://github.com/Temsters/cprg306-assignments.git" target="_blank" rel="noopener noreferrer"> <u>Temsters/cprg306-assignments</u>
+        <Link href="https://github.com/Temsters/cprg306-assignments.git"> <u>Temsters/cprg306-assignments</u>
         </Link>
       </p>
-    </div>
+    </main>
   );
 }
-
-export default StudentInfo;
