86085512da7386e47c2560ae18402145a7c90ab8
Temi Bankole <temibankole5@gmail.com>
2025-09-24 13:45:36 -0600

    text colour changes

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -3,10 +3,10 @@
 export default function Item({ name, quantity, category, bgColor }) {
   return (
     <div className={`my-2 max-w-sm p-2 rounded-lg mx-auto text-center shadow-md ${bgColor}`}>
-      <ul className="space-y-2">
-        <li><span className="font-semibold">Name:</span> {name}</li>
-        <li><span className="font-semibold">Quantity:</span> {quantity}</li>
-        <li><span className="font-semibold">Category:</span> {category}</li>
+      <ul className="space-y-2 ">
+        <li><span className="font-semibold text-white">Name:</span> {name}</li>
+        <li><span className="font-semibold text-white">Quantity:</span> {quantity}</li>
+        <li><span className="font-semibold text-white">Category:</span> {category}</li>
       </ul>
     </div>
   );
