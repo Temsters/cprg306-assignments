a3a0be2c94c711be2823636f4d2794f5a1943348
Temi Bankole <temibankole5@gmail.com>
2025-09-30 17:50:35 -0600

    add link to week four

--- app/page.js
+++ app/page.js
@@ -8,6 +8,7 @@ export default function Page() {
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p><Link href="/week-2">Go to week 2</Link></p>
       <p><Link href="/week-3">Go to week 3</Link></p>
+      <p><Link href="/week-4">Go to week 4</Link></p>
     </main>
   );
 }
