620113895fd6151c557140bb224ff9436b2dbbcb
Temi Bankole <temibankole5@gmail.com>
2025-10-02 08:46:00 -0600

    initialization of state variables, increment & decrement functions created

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,11 +1,26 @@
 "use client";
+import { useState } from "react";
 
 //New item component
 export default function NewItem() {
+
+  //State variable called quantity & setter called setQuantity
+  const [quantity, setQuantity] = useState(1);
+  
+  //Increment function to increase quantity by 1 with a max of 20
+  const increment = () => {
+    setQuantity(Math.min(quantity + 1, 20));
+  }
+
+  //Decrement function to decrease quantity by 1, with a min of 1
+  const decrement = () => {
+    setQuantity(Math.max(quantity - 1, 1));
+  }
+
   return (
-    <div>
-      <h2>Create a New Item</h2>
-      {/* Form or other content for creating a new item goes here */}
-    </div>
-  );
+    <p>Quantity: {quantity}</p>
+    
+  
+);
 }
+
