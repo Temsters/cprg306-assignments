5a85118f906083b5e3f60bd4561c462c4bdf3c00
Temi Bankole <temibankole5@gmail.com>
2025-10-03 12:26:01 -0600

    additional spacing changes

--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,8 +3,8 @@ import NewItem from "./new-item";
 
 export default function Page() {
   return (
-    <main className="space-y-2 space-x-0 text-blue-500">
-      <h1 className="max-w-screen my-3 text-3xl font-bold text-blue-500 pl-10 rounded-b p-3">Add New Item</h1>
+    <main className="space-y-3 space-x-0 text-blue-500">
+      <h1 className="max-w-screen my-0.5 text-3xl font-bold text-blue-500 pl-11 rounded-b p-3">Add New Item</h1>
       <NewItem />
     </main>
   );
