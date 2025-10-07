297dbfdf92da8c77a3d38172ae66c6d0885d0e04
Temi Bankole <temibankole5@gmail.com>
2025-10-07 16:51:35 -0600

    add link to week 5

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Page() {
       <p><Link href="/week-2">Go to week 2</Link></p>
       <p><Link href="/week-3">Go to week 3</Link></p>
       <p><Link href="/week-4">Go to week 4</Link></p>
+      <p><Link href="/week-5">Go to week 5</Link></p>
     </main>
   );
 }
