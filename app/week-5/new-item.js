07870fccaf9642b35abe012a58f0b735545d92b4
Temi Bankole <temibankole5@gmail.com>
2025-10-07 16:34:09 -0600

    copying files from week-4

---
+++ app/week-5/new-item.js
@@ -0,0 +1,55 @@
+"use client";
+import { useState } from "react";
+
+//New item component
+export default function NewItem() {
+
+  //State variable called quantity & setter called setQuantity
+  const [quantity, setQuantity] = useState(1);
+
+  //Constant to handle max quantity
+  const maxQuantity = 20;
+  //Constant to handle min quantity
+  const minQuantity = 1;
+  
+  //Increment function to increase quantity by 1 with a max of 20
+  const increment = () => {
+    setQuantity(Math.min(quantity + 1, maxQuantity));
+  }
+
+  //Decrement function to decrease quantity by 1, with a min of 1
+  const decrement = () => {
+    setQuantity(Math.max(quantity - 1, minQuantity));
+  }
+
+  return (
+  <div className=" max-w-md bg-blue-100 rounded-b lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
+    <p className=" text-blue-500 font-bold text-2xl mb-4 ">Quantity: {quantity}</p>
+    <div className="flex gap-4 mb-4 my-1 pl">
+      {/* Decrement button (-)s
+      //Calls decrement function on click and disabled if quantity is 1 */}
+      <button
+        onClick={decrement}
+        disabled={quantity === minQuantity}
+        className="bg-blue-500 text-white rounded p-4 border-2 disabled:bg-blue-300 disabled:cursor-not-allowed"
+        >
+          Decrement
+        </button>
+      {/* Increment button (+)
+      //Calls increment function on click and disabled if quantity is 20 */}
+      <button
+        onClick={increment}
+        disabled={quantity === maxQuantity}
+        className="bg-green-500 text-white rounded p-4 border-2 disabled:bg-green-300 disabled:cursor-not-allowed"
+      >
+        Increment
+      </button>
+    </div>
+     <p className="text-black italic font-serif">Allowed range: 1 - 20</p>
+  </div>
+
+  );
+
+
+}
+
---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+//week 4 page component that renders NewItem component
+import NewItem from "./new-item";  
+
+export default function Page() {
+  return (
+    <main className="space-y-3 space-x-0 text-blue-500">
+      <h1 className="max-w-screen my-0.5 text-3xl font-bold text-blue-500 pl-11 rounded-b p-3">Add New Item</h1>
+      <NewItem />
+    </main>
+  );
+}
