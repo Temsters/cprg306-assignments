e42c461b745b2d27176ed34a8949c6d9d8545ddd
Temi Bankole <temibankole5@gmail.com>
2025-10-07 20:55:36 -0600

    editing form submission file adding tailwind

--- app/components/SimpleFormSubmission.js
+++ app/components/SimpleFormSubmission.js
@@ -1,10 +1,42 @@
 "use client";
 import { useState } from "react";
 
+//Create handleSubmit function to handle form submission
 export default function HandleSubmit({ onSubmit }) {
-  
- //State variable called name & setter called setName
-  const [name, setName] = useState("");
 
-  //State variable called category and setter function called setCategory
-  const [category, setCategory] = useState("produce")
+  //State variables
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("produce");
+
+  //Handle form submission
+  const submitFunction = (event ) => {
+    event.preventDefault();
+
+  //Item object 
+    const item = {
+      name,
+      category
+    };
+
+    //Log item object to console
+    console.log(item);
+
+    //Display alert with current state of name quantity and category
+    alert(`Submitted: ${JSON.stringify(item)}`);
+
+
+    //Reset the state variables to their initial values
+    setName("");
+    setCategory("produce");
+  };
+
+    return (
+      //Create form with onSubmit event handler
+      <div>
+        <form className=" mb-4 border-2 border-blue-300 p-4 rounded bg-white" onSubmit={submitFunction}>
+          <input type="text" placeholder="e.g., milk, 4L🥛" value={name} onChange={(e) => setName(e.target.value)} required />
+        </form>
+      </div>
+    )
+  }
+
