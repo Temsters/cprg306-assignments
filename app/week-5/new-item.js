45f2af109a23863fe76fff318eb2ea230c0fc5a5
Temi Bankole <temibankole5@gmail.com>
2025-10-11 13:09:16 -0600

    dark mode styling added

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -15,11 +15,13 @@ export default function NewItem() {
 
   return (
     
-  <div className=" max-w-md bg-blue-100 rounded-b lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
+  <div className=" max-w-md bg-blue-100  dark:bg-gray-900 dark:border-blue-500 rounded-b lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
       {/* Form component */}
       <SimpleFormSubmission onDataSend={handleData} />
       {/* Submit button */}
-      <button type="submit" className="bg-blue-500 text-white rounded p-2 border-2 border-black transform hover:scale-105 transition duration-200 ease-in-out"> Add New Item </button>
+      <div className="flex justify-center">
+        <button type="submit" className="bg-blue-500 text-white rounded p-2 border-2 border-white dark:border-gray-700 transform hover:scale-105 transition duration-200 ease-in-out"> Add New Item </button>
+      </div>
   </div>
 
   );
