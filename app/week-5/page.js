6355d61ffdd769070478b762e8839c9eda7be0d3
Temi Bankole <temibankole5@gmail.com>
2025-10-11 12:25:46 -0600

    changing header title

--- app/week-5/page.js
+++ app/week-5/page.js
@@ -4,7 +4,7 @@ import NewItem from "./new-item";
 export default function Page() {
   return (
     <main className="space-y-3 space-x-0 text-blue-500">
-      <h1 className="max-w-screen my-0.5 text-3xl font-bold text-blue-500 pl-11 rounded-b p-3">Add New Item</h1>
+      <h1 className="max-w-screen my-0.5 text-3xl font-bold text-blue-500 pl-11 rounded-b p-3">Week 5 - New Item</h1>
       <NewItem />
     </main>
   );
