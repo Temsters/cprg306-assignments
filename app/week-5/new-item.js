2ca70a1ddc38e4bc1c47673383326fbc1191c84f
Temi Bankole <temibankole5@gmail.com>
2025-10-11 14:13:38 -0600

    fixing the add new item button

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -16,10 +16,6 @@ export default function NewItem() {
   <div className=" max-w-md bg-blue-100 dark:bg-gray-900 dark:border-blue-500 rounded-2xl lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
       {/* Form component */}
       <SimpleFormSubmission onDataSend={handleData} />
-      {/* Submit button */}
-      <div className="flex justify-center">
-        <button type="submit" className="bg-blue-200  dark:bg-blue-500  text-black dark:text-black rounded-2xl p-2 border-2 border-black transform hover:scale-105 transition duration-200 ease-in-out"> Add New Item </button>
-      </div>
   </div>
 
   );
