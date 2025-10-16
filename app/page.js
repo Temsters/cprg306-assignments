e49d6a94d4927eec8313ddc34ae953b38d315a54
Temi Bankole <temibankole5@gmail.com>
2025-10-15 23:00:41 -0600

    FIXING THE LINK

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <h1> CPRG 306: Web Development 2 - Assignments</h1>
       <p><Link href="/week-2">Go to week 2</Link></p>
       <p><Link href="/week-3">Go to week 3</Link></p>
       <p><Link href="/week-4">Go to week 4</Link></p>
