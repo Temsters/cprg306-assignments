a4eb5f795fd43e1428e7df34a6da8520b8b76c16
Temi Bankole <temibankole5@gmail.com>
2025-10-15 22:35:24 -0600

    spacing between the buttons and text

--- app/week-6/page.js
+++ app/week-6/page.js
@@ -5,7 +5,7 @@ import ItemList from "./item-list";
 export default function Page() {
   return (
     <main className="space-y-2 text-white">
-      <h1 className="my-12 text-3xl font-bold bg-[oklch(50%_0.134_242.749)] rounded-bl-lg text-center shadow-md text-white">Shopping List</h1>
+      <h1 className="my-12 text-3xl font-bold bg-[oklch(50%_0.134_242.749)] rounded-bl-lg text-center shadow-md text-white mb-5">Shopping List</h1>
       <ItemList />
     </main>
   );
