d4d80a3dc1c3668f6335fc90d4ab7e3c2bd64769
Temi Bankole <temibankole5@gmail.com>
2025-10-23 23:35:14 -0600

    linking the new-item file

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -18,7 +18,7 @@ export default function Page() {
 return (
  <main className="space-y-2 text-white">
       <h1 className="my-12 text-3xl font-bold bg-[oklch(50%_0.134_242.749)] rounded-bl-lg text-center shadow-md text-white mb-5">
-        Week 7- Shopping List
+        Week 7 - Shopping List
       </h1>
 
       {/* Pass handleAddItem to NewItem as a prop */}
