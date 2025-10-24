652798b3ce75fef18db5612c2a826308108c8fa4
Temi Bankole <temibankole5@gmail.com>
2025-10-24 00:46:01 -0600

    changing background colours

--- app/week-7/new-item.js
+++ app/week-7/new-item.js
@@ -15,7 +15,7 @@ export default function NewItem({ onAddItem }) {
   }
 
   return (
-  <div className=" max-w-md bg-blue-100 dark:bg-gray-900 dark:border-blue-500 rounded-2xl lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
+  <div className=" max-w-md bg-blue-500 dark:bg-gray-900 border-[oklch(50%_0.134_242.749)] rounded-2xl lg:rounded ml-5 p-6 pt-6 border-2">
       {/* Form component */}
       <SimpleFormSubmission onAddItem={handleAddItem} />
   </div>
