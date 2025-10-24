fa40f741ef036b67fafadc850505e976543bf2c4
Temi Bankole <temibankole5@gmail.com>
2025-10-23 23:59:01 -0600

    event handler function and passing onAddItem as a prop

--- app/week-7/new-item.js
+++ app/week-7/new-item.js
@@ -3,19 +3,21 @@ import SimpleFormSubmission from "../components/SimpleFormSubmission";
 import { useState } from "react";
 
 //New item component
-export default function NewItem() {
-
+export default function NewItem({ onAddItem }) {
 
   //State variable to handle form data
   const [formData, setFormData] = useState("");
-  function handleData(data) {
-    setFormData(data);
+
+  //Event handler function to add new item using onAddItem prop
+  function handleAddItem(item) {
+    setFormData(item);
+    onAddItem(item);
   }
 
   return (
   <div className=" max-w-md bg-blue-100 dark:bg-gray-900 dark:border-blue-500 rounded-2xl lg:rounded ml-5 p-6 pt-6 border-2 border-blue-300">
       {/* Form component */}
-      <SimpleFormSubmission onDataSend={handleData} />
+      <SimpleFormSubmission onAddItem={handleAddItem} />
   </div>
 
   );
