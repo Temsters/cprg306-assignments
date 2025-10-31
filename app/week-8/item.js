8af6e95772453bffbd80fdeefd675650ea4e4079
Temi Bankole <temibankole5@gmail.com>
2025-10-30 22:50:26 -0600

    adding tailwind hover state to the buttons for ingredient items

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -2,7 +2,7 @@
 //item component accepting name, quantity, category as props
 export default function Item({ name, quantity, category, bgColor, onSelect }) {
   return (
-    <div onClick={onSelect} className={`my-2 max-w-sm p-2 rounded-lg mx-auto text-center shadow-md ${bgColor}  dark:border-white`}>
+    <div onClick={onSelect} className={`cursor-pointer hover:scale-105 transition-transform duration-200 my-2 max-w-sm p-2 rounded-lg mx-auto text-center shadow-md ${bgColor} dark:border-white`}>
       <ul className="space-y-2 ">
         <li><span className="font-semibold text-white">Name:</span> {name}</li>
         <li><span className="font-semibold text-white">Quantity:</span> {quantity}</li>
