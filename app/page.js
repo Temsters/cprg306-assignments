8bfe65ff81d867066f88e2ef3609ebe41d602ec5
Temi Bankole <temibankole5@gmail.com>
2025-11-10 17:00:18 -0700

    linking to week 9 page

--- app/page.js
+++ app/page.js
@@ -13,6 +13,7 @@ export default function Page() {
       <p><Link href="/week-6">Go to week 6</Link></p> 
       <p><Link href="/week-7">Go to week 7</Link></p>
       <p><Link href="/week-8">Go to week 8</Link></p>
+      <p><Link href="/week-9">Go to week 9</Link></p>
     </main>
   );
 }
