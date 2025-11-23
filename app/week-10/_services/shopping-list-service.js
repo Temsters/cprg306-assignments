7b6d19b7612988f189e625fd429b2dd67f464fcf
Temi Bankole <temibankole5@gmail.com>
2025-11-22 19:39:30 -0700

    addItem function

--- app/week-10/_services/shopping-list-service.js
+++ app/week-10/_services/shopping-list-service.js
@@ -12,11 +12,10 @@ export async function getItems(userId) {
     const itemsQuery = query(collection(db, 'users', userId, 'items'));
     const querySnapshot =  await getDocs(itemsQuery);
     const items = [];
-    //Fetches the documents from the items subcollection and for each document adds an object to the items array 
+    //Fetches the documents from the items subcollection and for each document adds an object to the items array containing the document id and data
     querySnapshot.forEach((doc) => {
       items.push({ id: doc.id, ...doc.data() });
     });
-    //Returns the items array
     return items;
   } catch (error) {
     console.error("Error getting items: ", error);
@@ -24,4 +23,17 @@ export async function getItems(userId) {
   }
 }
 
-//test
+//Add the addItem async function
+export async function addItem(userId, item) {
+  try {
+    //Uses userId to reference the items subcollection under the user's document in the 'users' collection
+    const itemsCollectionRef = collection(db, 'users', userId, 'items');
+    //Adds the item to this subcollection
+    const docRef = await addDoc(itemsCollectionRef, item);
+    //Returns id of the newly added document and item data.
+    return { id: docRef.id, ...item };
+  } catch (error) {
+    console.error("Error adding item: ", error);
+    return null;
+  }
+}
